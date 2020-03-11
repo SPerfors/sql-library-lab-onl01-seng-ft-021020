1f79b018e8c8380a74430af3692e1539ced8416f
Steven Perfors <stevecanbuild@gmail.com>
2020-03-11 18:40:29 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -14,3 +14,24 @@ CREATE TABLE authors (
   id PRIMARY INTEGER KEY,
   name TEXT
 );
+
+CREATE TABLE books (
+  id PRIMARY INTEGER KEY,
+  title TEXT, 
+  year INTEGER,
+  series_id INTEGER
+);
+
+CREATE TABLE characters (
+  id PRIMARY INTEGER KEY,
+  name TEXT, 
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER
+);
+
+CREATE TABLE character_books (
+  id PRIMARY INTEGER KEY,
+  book_id INTEGER,
+  character_id INTEGER
+);
