d2ecb132b93b1f1aa82c7901483d901d2a4ad7d3
Steven Perfors <stevecanbuild@gmail.com>
2020-03-11 18:42:26 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,29 +1,29 @@
 CREATE TABLE series (
-  id PRIMARY INTEGER KEY,
+  id INTEGER PRIMARY KEY,
   title TEXT, 
   author_id INTEGER,
   subgenre_id INTEGER
 );
 
 CREATE TABLE subgenre (
-  id PRIMARY INTEGER KEY,
+  id INTEGER PRIMARY KEY,
   name TEXT
 );
 
 CREATE TABLE authors (
-  id PRIMARY INTEGER KEY,
+  id INTEGER PRIMARY KEY,
   name TEXT
 );
 
 CREATE TABLE books (
-  id PRIMARY INTEGER KEY,
+  id INTEGER PRIMARY KEY,
   title TEXT, 
   year INTEGER,
   series_id INTEGER
 );
 
 CREATE TABLE characters (
-  id PRIMARY INTEGER KEY,
+  id INTEGER PRIMARY KEY,
   name TEXT, 
   motto TEXT,
   species TEXT,
@@ -31,7 +31,7 @@ CREATE TABLE characters (
 );
 
 CREATE TABLE character_books (
-  id PRIMARY INTEGER KEY,
+  id INTEGER PRIMARY KEY,
   book_id INTEGER,
   character_id INTEGER
 );
