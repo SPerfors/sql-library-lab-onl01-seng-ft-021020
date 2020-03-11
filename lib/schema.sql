b32efa086f22672b4f42d9e529d027b108a91e5d
Steven Perfors <stevecanbuild@gmail.com>
2020-03-11 18:38:11 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,3 +4,13 @@ CREATE TABLE series (
   author_id INTEGER,
   subgenre_id INTEGER
 );
+
+CREATE TABLE subgenre (
+  id PRIMARY INTEGER KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id PRIMARY INTEGER KEY,
+  name TEXT
+);
