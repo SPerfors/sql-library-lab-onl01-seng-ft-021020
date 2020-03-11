168a172af3c45ce5a503d0e07d11d545faf4ffe7
Steven Perfors <stevecanbuild@gmail.com>
2020-03-11 18:51:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
 INSERT INTO series (id) VALUES (1), (2);
 INSERT INTO subgenres (id) VALUES (1), (2);
 INSERT INTO authors (id) VALUES (1), (2);
-INSERT INTO books (series_id) VALUES (1), (2), (3);
+INSERT INTO books (series_id) VALUES (1), (2), (3), (4), (5), (6);
